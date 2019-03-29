376b74192138ac0ef6284f95c6172b92bb684af2
Thiago Bignotto <bignotto@gmail.com>
2019-03-29 13:42:39 -0300

    user login

---
+++ src/credentials/_index.js
@@ -0,0 +1,9 @@
+const credential = {};
+
+credential.database = {
+    'dataBaseUser': 'the data base user',
+    'dataBasePass': 'the data base password',
+    'dataBaseUrl': 'the data base url for connections'
+}
+
+module.exports = credential;
--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://developer:dev123@authtest-auk5w.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect('mongodb+srv://developer:dev123@authtest-auk5w.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true, useCreateIndex: true }, (err) => {
+    if(err) console.log(err);
+});
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
